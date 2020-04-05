6312fc3278ad08e55006d7cc1be5ba98465dfa0e
Stephen Callum <stephen.callum@hotmail.co.uk>
2020-04-05 21:04:42 +0100

    Initial commit

---
+++ Source/CustomVehiclePhysics/CustomVehiclePhysicsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomVehiclePhysicsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMVEHICLEPHYSICS_API ACustomVehiclePhysicsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
